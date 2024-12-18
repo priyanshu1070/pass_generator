40a5741c85b84e39d7824c2e3fab9a9296d62763
Priyanshu <priyanshu4pal@gmail.com>
2024-12-18 19:15:06 +0530

    pass-gen v.1

---
+++ script.js
@@ -0,0 +1,42 @@
+const upper_case = document.getElementById("upperCase");
+const lower_case = document.getElementById("lowerCase");
+const num = document.getElementById("Number");
+const Symbol = document.getElementById("Symbol");
+const passLength = document.getElementById("length")
+const btn = document.getElementById("generate");
+const output = document.getElementById("display");
+const lowercase ="qwertyuiopasdfghjklzxcvbnm";
+const uppercase ="QWERTYUIOPASDFGHJKLZXCVBNM";
+const numbers = "0987654321";
+// const symbols = "@%$";
+const symbols = "~`!@#$%^&*()_+{}:?/.';><";
+btn.onclick=click;
+function click(){
+    let result = "";
+    let passlength = passLength.value;
+    let allowedChar="";
+    let rand_index;
+    
+    if (upper_case.checked){
+        allowedChar+=uppercase;
+    }
+    if (lower_case.checked){
+        allowedChar+=lowercase;
+    }
+    if (num.checked){
+        allowedChar+=numbers;
+    }
+    if (Symbol.checked){
+        allowedChar+=symbols;
+    }
+    else if (!(upper_case.checked||lower_case.checked||num.checked||Symbol.checked)){
+        window.alert("Select any one case");
+        output.value=" ";
+        return;
+    }
+    for (let i=0; i<passlength;i++){
+        rand_index=Math.floor(Math.random()*allowedChar.length);
+        result+=allowedChar[rand_index];
+    output.value=result;
+    }
+}
